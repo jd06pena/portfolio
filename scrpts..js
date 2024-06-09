541556f2f4e0a33d3f18f7da51ecf70bec824132
juan peña <jd.pena06@ciaf.edu.co>
2024-06-09 05:17:09 -0500

    se subio

---
+++ scrpts..js
@@ -0,0 +1,9 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.getElementById('menu-icon');
+    const navLinks = document.getElementById('nav-links');
+
+    menuIcon.addEventListener('click', function() {
+        navLinks.classList.toggle('show');
+    });
+});
